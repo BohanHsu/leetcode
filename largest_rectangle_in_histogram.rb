ee41b66c45198c03de0bee65b6f9fe49921d404a
BohanHsu <ilovebarce@gmail.com>
2015-05-31 12:08:47 -0400

    largest rectangle in histrogram pass local test. O(n logn)

--- largest_rectangle_in_histogram.rb
+++ largest_rectangle_in_histogram.rb
@@ -1,37 +1,83 @@
+### @param {Integer[]} height
+### @return {Integer}
+##def largest_rectangle_area(height)
+##  stack = []
+##  max = nil
+##
+##  height.each_with_index do |elem, i|
+##    if stack.empty? || elem >= height[stack.last]
+##      stack << i 
+##    else
+##      while !stack.empty? && height[stack.last] > elem do
+##        bgn = stack.pop
+##        local_max = (i - bgn) * height[bgn]
+##
+##        if max.nil?
+##          max = local_max
+##        else
+##          max = local_max if local_max > max
+##        end
+##      end
+##      stack << i
+##    end
+##  end
+##
+##  while !stack.empty? do
+##    bgn = stack.pop
+##    local_max = (height.length - bgn) * height[bgn]
+##
+##    if max.nil?
+##      max = local_max
+##    else
+##      max = local_max if local_max > max
+##    end
+##  end
+##
+##  max.nil? ? 0 : max
+##end
+
 # @param {Integer[]} height
 # @return {Integer}
 def largest_rectangle_area(height)
-  stack = []
+  return 0 if height.empty?
   max = nil
-
-  height.each_with_index do |cur_e, cur_i|
-    if stack.empty? || cur_e >= height[stack.last]
-      stack << cur_i 
-    else
-      while !stack.empty? && height[stack.last] > cur_e do
-        dist = stack.pop
-
-      end
-      lower_bound = stack.empty? ? -1 : stack.last
-      dist = cur_i - lower_bound
-      local_max = dist * cur_e
-      if max.nil?
-        max = local_max
-      else
-        max = local_max if local_max > max
-      end
-    end
-  end
-
-  stack.each do |i|
-    dist = stack.last - i + 1
-    local_max = dist * height[i]
+  s = []
+  s.push [0, height.length]
+  j = 0
+  while !s.empty? && j < 5 do
+    from = s.last[0]
+    to = s.pop[1]
+    middle = min_index(height, from, to)
+    area = height[middle] * (to - from)
     if max.nil?
-      max = local_max
+      max = area
     else
-      max = local_max if local_max > max
+      max = area if area > max
+    end
+
+    s.push [from, middle] if middle > from
+    s.push [middle + 1, to] if to > middle + 1
+    
+    j += 1
+  end
+  max
+end
+
+def min_index(arr, from, to)
+  index = nil
+  min = nil
+  arr.each_with_index do |e, i|
+    if i >= from && i < to
+      if min.nil?
+        min = e
+        index = i
+      else
+        if e < min
+          min = e
+          index = i
+        end
+      end
     end
   end
-
-  max.nil? ? 0 : max
+  index
 end
--- test/test_largest_rectangle_in_histogram.rb
+++ test/test_largest_rectangle_in_histogram.rb
@@ -2,21 +2,21 @@ require 'minitest/autorun'
 require '../largest_rectangle_in_histogram'
 
 describe 'largest_rectangle_area' do
-  #it 'should pass test 1' do
-  #  largest_rectangle_area([2,1,5,6,2,3]).must_equal(10)
-  #end
+  it 'should pass test 1' do
+    largest_rectangle_area([2,1,5,6,2,3]).must_equal(10)
+  end
 
-  #it 'should pass test 2 empty array' do
-  #  largest_rectangle_area([]).must_equal(0)
-  #end
+  it 'should pass test 2 empty array' do
+    largest_rectangle_area([]).must_equal(0)
+  end
 
-  #it 'should pass test 3' do
-  #  largest_rectangle_area([1]).must_equal(1)
-  #end
+  it 'should pass test 3' do
+    largest_rectangle_area([1]).must_equal(1)
+  end
 
-  #it 'should pass test 4' do
-  #  largest_rectangle_area([1,2,3,4]).must_equal(6)
-  #end
+  it 'should pass test 4' do
+    largest_rectangle_area([1,2,3,4]).must_equal(6)
+  end
 
   it 'should pass test 5' do
     largest_rectangle_area([9,0]).must_equal(9)
@@ -24,5 +24,15 @@ describe 'largest_rectangle_area' do
 
   it 'should pass test 6' do
     largest_rectangle_area([8,9,0]).must_equal(16)
+  end
+
+  it 'should pass test 7' do
+    largest_rectangle_area([2,1,2]).must_equal(3)
+  end
+end
+
+describe "min_index" do
+  it "should fml" do
+    min_index([2,1,2], 0, 3).must_equal(1)
   end
 end
