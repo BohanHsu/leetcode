bbdd06527a1ae5d294e43ce0ce931ab01c74e617
BohanHsu <ilovebarce@gmail.com>
2015-06-07 11:38:44 -0400

    not pass leetcode num_rows=2 test, dont know why

--- test/test_zigzag_conversion.rb
+++ test/test_zigzag_conversion.rb
@@ -2,11 +2,71 @@ require 'minitest/autorun'
 require '../zigzag_conversion.rb'
 
 describe 'calculate_char_num_in_line' do
-  #it 'should pass test 1' do
-  #  calculate_char_num_in_line(16, 5).must_equal([2,4,4,4,2])
-  #end
+  it 'should pass test 1' do
+    calculate_char_num_in_line(16, 5).must_equal([2,4,4,4,2])
+  end
 
   it 'should pass test 1' do
     calculate_char_num_in_line(17, 5).must_equal([3,4,4,4,2])
+  end
+
+  it 'should pass test 2' do
+    calculate_char_num_in_line(18, 5).must_equal([3,5,4,4,2])
+  end
+
+  it 'should pass test 3' do
+    calculate_char_num_in_line(19, 5).must_equal([3,5,5,4,2])
+  end
+
+  it 'should pass test 4' do
+    calculate_char_num_in_line(20, 5).must_equal([3,5,5,5,2])
+  end
+
+  it 'should pass test 5' do
+    calculate_char_num_in_line(21, 5).must_equal([3,5,5,5,3])
+  end
+
+  it 'should pass test 6' do
+    calculate_char_num_in_line(22, 5).must_equal([3,5,5,6,3])
+  end
+
+  it 'should pass test 7' do
+    calculate_char_num_in_line(23, 5).must_equal([3,5,6,6,3])
+  end
+
+  it 'should pass test 8' do
+    calculate_char_num_in_line(24, 5).must_equal([3,6,6,6,3])
+  end
+  
+  it 'should pass test 9' do
+    calculate_char_num_in_line(25, 5).must_equal([4,6,6,6,3])
+  end
+end
+
+describe 'calculate_accumulated_num_in_line' do
+  it 'should pass test 1' do
+    char_num_in_line = calculate_char_num_in_line(25, 5)
+    char_num_in_line.must_equal([4,6,6,6,3])
+    calculate_accumulated_num_in_line(char_num_in_line).must_equal([4,10,16,22,25])
+  end
+end
+
+describe 'convert' do
+  it 'should pass test 1' do
+    s = 'aibhjqcgkpdfloen'
+    num_rows = 5
+    convert(s, num_rows).must_equal('abcdefghijklnopq')
+  end
+
+  it 'should pass test 2' do
+    s = 'airbhjqscgkptdflouenv'
+    num_rows = 5
+    convert(s, num_rows).must_equal('abcdefghijklnopqrstuv')
+  end
+
+  it 'should pass test3 corner case' do
+    s = ''
+    num_rows = 1
+    convert(s, num_rows).must_equal('')
   end
 end
--- zigzag_conversion.rb
+++ zigzag_conversion.rb
@@ -17,8 +17,36 @@
 # @param {Integer} num_rows
 # @return {String}
 def convert(s, num_rows)
+  return s if num_rows == 1
   modulee = num_rows * 2 - 2
-  i_last = s.length - 1
+  accumulated = calculate_accumulated_num_in_line(calculate_char_num_in_line(s.length, num_rows))
+  cur_line = 0
+  arr = s.length.times.map {nil}
+  s.chars.each_with_index do |c, i|
+    if i < accumulated[cur_line]
+    elsif i == accumulated[cur_line]
+      cur_line += 1
+    end
+
+    y = cur_line
+    if cur_line == 0
+      x = i
+      new_index = x * modulee + y
+    elsif cur_line == num_rows - 1
+      x = i - accumulated[cur_line-1]
+      new_index = x * modulee + y
+    else
+      x = (i - accumulated[cur_line-1]) / 2
+      if (i - accumulated[cur_line-1]) % 2 == 0
+        new_index = x * modulee + y
+      else
+        new_index = (x + 1) * modulee - y
+      end
+    end
+    arr[new_index] = c
+    #p "i=#{i}, c=#{c}, x=#{x}, y=#{y}, new_index=#{new_index}, arr=#{arr}"
+  end
+  arr.join ""
 end
 
 def calculate_char_num_in_line(length, num_rows)
@@ -29,15 +57,27 @@ def calculate_char_num_in_line(length, num_rows)
       nums = length / modulee
       nums += 1 if length % modulee != 0
     elsif offset == num_rows - 1
-      nums = char_num_in_line[0]
-      nums +=1 if length % modulee >= num_rows
+      nums = char_num_in_line[0] - 1
+      nums +=1 if (length % modulee >= num_rows) || length % modulee == 0
     else
-      nums = length / modulee * 2 - 2
-      p "length=#{length}, modulee=#{modulee}, nums=#{nums}"
-      nums += 1 if length % modulee >= offset || length % modulee == 0
-      nums += 1 if length % modulee >= modulee - offset || length % modulee == 0
+      nums = length / modulee * 2
+      ofs = length % modulee
+      nums += 2 if ofs + offset > modulee
+      nums += 1 if ofs + offset <= modulee && offset < ofs
     end
     char_num_in_line[offset] = nums
   end
   char_num_in_line
+end
+
+def calculate_accumulated_num_in_line(char_num_in_line)
+  accumulated = char_num_in_line.map {nil}
+  char_num_in_line.each_with_index do |e, i|
+    if i == 0
+      accumulated[i] = char_num_in_line[i]
+    else
+      accumulated[i] = e + accumulated[i-1]
+    end
+  end
+  accumulated
 end
