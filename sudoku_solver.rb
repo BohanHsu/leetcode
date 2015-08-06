938d01d9bdee098bee01b0bfeb10b991c4cd2908
BohanHsu <ilovebarce@gmail.com>
2015-08-06 17:07:02 -0400

    passed

--- sudoku_solver.rb
+++ sudoku_solver.rb
@@ -5,10 +5,7 @@
 # @param {Character[][]} board
 # @return {Void} Do not return anything, modify board in-place instead.
 def solve_sudoku(board)
-  #board.map! do |row|
-  #  row.split //
-  #end
-  q = Queue.new
+  q = []
   first_coord = next_coordinate(board, 0, -1)
   first_values = all_possible_value(board, first_coord.first, first_coord.last)
   first_values.each do |val|
@@ -19,7 +16,16 @@ def solve_sudoku(board)
     n = q.pop
     byebug unless n[:board].length == 9
     coord = next_coordinate(n[:board], n[:i], n[:j])
-    return n[:board].map { |row| row.join "" } if coord.nil?
+    if coord.nil?
+      height = n[:board].length
+      width = n[:board][0].length
+      (0...height).each do |i|
+        (0...width).each do |j|
+          board[i][j] = n[:board][i][j]
+        end
+      end
+      return
+    end
     i = coord.first
     j = coord.last
 
@@ -67,90 +73,3 @@ def next_coordinate(board, i, j)
   end
   nil
 end
-
-#def solve_sudoku(board)
-#  q = Queue.new
-#  first_coord = next_coordinate(board, 0, -1)
-#  f_i = first_coord.first
-#  f_j = first_coord.last
-#
-#  first_values = all_possible_value(board, nil, f_i, f_j)
-#
-#  first_values.each do |val|
-#    q << {:i => f_i, :j => f_j, :val => val }
-#  end
-#
-#  until q.empty? do
-#    node = q.pop
-#
-#    next_coord = next_coordinate(board, node[:i], node[:j])
-#    if next_coord.nil?
-#      return fill_board(board, node)
-#    end
-#
-#    next_i = next_coord.first
-#    next_j = next_coord.last
-#
-#    next_values = all_possible_value(board, node, next_i, next_j)
-#
-#    next_values.each do |val|
-#      q << { :i => next_i, :j => next_j, :val => val, :prev => node }
-#    end
-#  end
-#end
-#
-#def fill_board(board, node)
-#  until node.nil? do
-#    board[node[:i]][node[:j]] = node[:val]
-#    node = node[:prev]
-#  end
-#  board
-#end
-#
-## board, 0, -1 will return first coordinate
-#def next_coordinate(board, i, j)
-#  ((j+1)...board[i].length).each do |new_j|
-#    return [i, new_j] if board[i][new_j] == '.'
-#  end
-#  ((i+1)...board.length).each do |new_i|
-#    board[new_i].length.times do |new_j|
-#      return [new_i, new_j] if board[new_i][new_j] == '.'
-#    end
-#  end
-#  nil
-#end
-#
-#def all_possible_value(board, node, coord_i, coord_j)
-#  occupied = {}
-#  until node.nil? do
-#    occupied[node[:i]] = {} if occupied[node[:i]].nil?
-#    occupied[node[:i]][node[:j]] = node[:val]
-#    node = node[:prev]
-#  end
-#
-#  values = (1..9).map do |i|
-#    i.to_s
-#  end
-#
-#  board.length.times do |i|
-#    values.delete(board[i][coord_j]) unless board[i][coord_j].nil?
-#    values.delete(occupied[i][coord_j]) unless occupied[i].nil? || occupied[i][coord_j].nil?
-#  end
-#
-#  board[coord_i].length.times do |j|
-#    values.delete(board[coord_i][j]) unless board[coord_i][j].nil?
-#    values.delete(occupied[coord_i][j]) unless occupied[coord_i].nil? || occupied[coord_i][j].nil?
-#  end
-#
-#  3.times do |i|
-#    3.times do |j|
-#      new_i = (coord_i / 3) * 3 + i
-#      new_j = (coord_j / 3) * 3 + j
-#
-#      values.delete(board[new_i][new_j]) unless board[new_i][new_j].nil?
-#      values.delete(occupied[new_i][new_j]) unless occupied[new_i].nil? || occupied[new_i][new_j].nil?
-#    end
-#  end
-#
-#  values
-#end
--- test/test_sudoku_solver.rb
+++ test/test_sudoku_solver.rb
@@ -2,76 +2,16 @@ require 'minitest/autorun'
 require '../sudoku_solver'
 
 describe 'solve_sudoku(board)' do
-  before do
-    @raw_board = [
-      [5,3,0,0,7,0,0,0,0],
-      [6,0,0,1,9,5,0,0,0],
-      [0,9,8,0,0,0,0,6,0],
-      [8,0,0,0,6,0,0,0,3],
-      [4,0,0,8,0,3,0,0,1],
-      [7,0,0,0,2,0,0,0,6],
-      [0,6,0,0,0,0,2,8,0],
-      [0,0,0,4,1,9,0,0,5],
-      [0,0,0,0,8,0,0,7,9]
-    ].map do |row|
-      row.map do |i|
-        if i == 0
-          '.'
-        else
-          i.to_s
-        end
-      end
+  it 'should work' do
+    board = ["..9748...","7........",".2.1.9...","..7...24.",".64.1.59.",".98...3..","...8.3.2.","........6","...2759.."].map do |str|
+      str.chars
     end
 
-    @expected_results = [
-      [5,3,4,6,7,8,9,1,2],
-      [6,7,2,1,9,5,3,4,8],
-      [1,9,8,3,4,2,5,6,7],
-      [8,5,9,7,6,1,4,2,3],
-      [4,2,6,8,5,3,7,9,1],
-      [7,1,3,9,2,4,8,5,6],
-      [9,6,1,5,3,7,2,8,4],
-      [2,8,7,4,1,9,6,3,5],
-      [3,4,5,2,8,6,1,7,9]
-    ].map do |row|
-      row.map do |i|
-        i.to_s
-      end
-    end.map do |row|
-      row.join ""
+    expected_result = ["519748632","783652419","426139875","357986241","264317598","198524367","975863124","832491756","641275983"].map do |str|
+      str.chars
     end
-  end
 
-  it 'should solve soduko' do
-    result = solve_sudoku(@raw_board)
-    result[0].must_equal(@expected_results[0])
-    result[1].must_equal(@expected_results[1])
-    result[2].must_equal(@expected_results[2])
-    result[3].must_equal(@expected_results[3])
-    result[4].must_equal(@expected_results[4])
-    result[5].must_equal(@expected_results[5])
-    result[6].must_equal(@expected_results[6])
-    result[7].must_equal(@expected_results[7])
-    result[8].must_equal(@expected_results[8])
-  end
-
-  it 'should return result' do
-    board = [
-      ".....7..9",
-      ".4..812..",
-      "...9...1.",
-      "..53...72",
-      "293....5.",
-      ".....53..",
-      "8...23...",
-      "7...5..4.",
-      "531.7...."]
-    board.map! do |row|
-      row.split //
-    end
-    p board
-
-    result = solve_sudoku(board)
-    p result
+    solve_sudoku(board)
+    board.must_equal(expected_result)
   end
 end
